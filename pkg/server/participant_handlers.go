12f7ba0f8d1eb7ae453dc1099da8e691909a8621
Sai Kiran Dasika <kirandasika30@gmail.com>
2018-11-17 12:46:59 -0600

    pkg/server: updated RPC definition

--- pkg/server/participant_handlers.go
+++ pkg/server/participant_handlers.go
@@ -3,16 +3,28 @@ package server
 import (
 	"context"
 
+	"github.com/auburnhacks/sponsor/pkg/participant"
 	api "github.com/auburnhacks/sponsor/proto"
+	log "github.com/sirupsen/logrus"
 )
 
 // ListParticipants is a method on the SponsorServer that lists all the participants
 // are synced from the external database
-func (s *rpcServer) ListParticipants(ctx context.Context, req *api.ListParticipantsRequest) (*api.ListParticipantsResponse, error) {
-	// pSlice, err := participant.List()
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// pbParticipants := []*api.Participant{}
-	return &api.ListParticipantsResponse{}, nil
+func (s *rpcServer) ListParticipants(ctx context.Context,
+	req *api.ListParticipantsRequest) (*api.ListParticipantsResponse, error) {
+	pSlice, err := participant.List()
+	if err != nil {
+		return nil, err
+	}
+	log.Debugf("%+v", pSlice)
+	// prp = protobufParticipants
+	prp := make([]*api.Participant, len(pSlice))
+	i := 0
+	for _, p := range pSlice {
+		log.Debugf("%v", p)
+		prp[i] = &api.Participant{}
+	}
+	return &api.ListParticipantsResponse{
+		Participants: prp,
+	}, nil
 }

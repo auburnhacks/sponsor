eb8b99349c982c12f8afdd76cef44e62015018cd
Sai Kiran Dasika <kirandasika30@gmail.com>
2018-12-06 23:35:10 -0600

    updated tests

--- pkg/utils/interceptors_test.go
+++ pkg/utils/interceptors_test.go
@@ -2,9 +2,17 @@ package utils
 
 import "testing"
 
-func TestIsUthenticatedRPC(t *testing.T) {
+func TestTrueUnauthenticatedRPCCall(t *testing.T) {
 	fullMethod := "/proto.SponsorService/LoginAdmin"
 	if err := isUnauthenticatedRPC(fullMethod); err != nil {
 		t.Fatalf("%v", err)
+	}
+}
+
+func TestInvalidRPCCall(t *testing.T) {
+	fullMethod := "/proto.SponsorService/FooBar"
+	err := isUnauthenticatedRPC(fullMethod)
+	if err == nil {
+		t.Fail()
 	}
 }

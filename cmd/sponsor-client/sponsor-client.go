b71de28119028719173097f5592fb27278386d26
Sai Kiran Dasika <kirandasika30@gmail.com>
2018-12-06 23:45:11 -0600

    cmd/sponsor-client: added basic server functionality

--- cmd/sponsor-client/sponsor-client.go
+++ cmd/sponsor-client/sponsor-client.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+	"net/http"
+	"time"
+)
+
+var (
+	assets     *string
+	listenAddr *string
+)
+
+func init() {
+	assets = flag.String("assets", "./static", "Path to the assets after \"ng build\"")
+	listenAddr = flag.String("listen_addr", "localhost:9000", "Listening addr for the server")
+
+	flag.Parse()
+}
 
 func main() {
-	fmt.Println("sponsor-client")
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(*assets)))
+	mux.HandleFunc("/_ready", ready)
+	mux.HandleFunc("/_healthz", health)
+
+	srv := &http.Server{
+		Addr:        *listenAddr,
+		ReadTimeout: 5 * time.Second,
+		Handler:     mux,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
+
+func ready(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("ok\n"))
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("ok\n"))
 }
